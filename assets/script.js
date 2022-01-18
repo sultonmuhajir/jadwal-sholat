103cd2d3cea07ee2ef60e8513c0672b79be10413
Sulton Muhajir <sulton.muhajir28@gmail.com>
2022-01-18 19:19:31 +0700

    + fetch

--- assets/script.js
+++ assets/script.js
@@ -1,45 +1,50 @@
 const tgl = new Date().toISOString().slice(0, 10).split("-");
-const kotaDefault = "bangkalan";
 
 
 function jadwalSholat(kota) {
    const tittle = kota.charAt(0).toUpperCase() + kota.slice(1);
-   $("h1").html(tittle);
-   $.getJSON(`https://raw.githubusercontent.com/lakuapik/jadwalsholatorg/master/adzan/${kota}/${tgl[0]}/${tgl[1]}.json`,
-      function (data) {
-         $.each(data, function (i, data) {
-            $("tbody").append(`<tr>
-            <td> ${data.tanggal.split("-").reverse().join("/")} </td>
-            <td> ${data.imsyak} </td>
-            <td> ${data.shubuh} </td>
-            <td> ${data.terbit} </td>
-            <td> ${data.dhuha} </td>
-            <td> ${data.dzuhur} </td>
-            <td> ${data.ashr} </td>
-            <td> ${data.magrib} </td>
-            <td> ${data.isya} </td>
-            </tr>`);
+   document.querySelector("h1").innerHTML = `${tittle} - ${tgl[1]}/${tgl[0]}`;
+   fetch(`https://raw.githubusercontent.com/lakuapik/jadwalsholatorg/master/adzan/${kota}/${tgl[0]}/${tgl[1]}.json`)
+      .then(res => res.json())
+      .then(data => {
+         let dataJadwal = ''
+         data.forEach(el => {
+            dataJadwal += `<tr>
+            <td> ${el.tanggal.split("-").reverse().join("/")} </td>
+            <td> ${el.imsyak} </td>
+            <td> ${el.shubuh} </td>
+            <td> ${el.terbit} </td>
+            <td> ${el.dhuha} </td>
+            <td> ${el.dzuhur} </td>
+            <td> ${el.ashr} </td>
+            <td> ${el.magrib} </td>
+            <td> ${el.isya} </td>
+            </tr>`
          })
-      })
+         document.querySelector("tbody").innerHTML = dataJadwal;
+      });
 }
-jadwalSholat(kotaDefault);
+jadwalSholat('bangkalan');
 
 
-$.getJSON("https://raw.githubusercontent.com/lakuapik/jadwalsholatorg/master/kota.json",
-   function (city) {
-      $.each(city, function (i, city) {
-         const cityTittle = city.charAt(0).toUpperCase() + city.slice(1);
-         if (city == "bangkalan") {
-            $("select").append(`<option value=${city} selected> ${cityTittle} </option>`);
+fetch('https://raw.githubusercontent.com/lakuapik/jadwalsholatorg/master/kota.json')
+   .then(res => res.json())
+   .then(data => {
+      let dataKota = ''
+      data.forEach(el => {
+         const cityTittle = el.charAt(0).toUpperCase() + el.slice(1);
+         if (el == 'bangkalan') {
+            dataKota += `<option value=${el} selected> ${cityTittle} </option>`
          } else {
-            $("select").append(`<option value=${city}> ${cityTittle} </option>`);
+            dataKota += `<option value=${el}> ${cityTittle} </option>`
          }
-      })
+      });
+      document.querySelector("select").innerHTML = dataKota
    })
 
 
-$("select").on("change", function () {
-   let citySelected = $("select").val();
-   $("tbody").html("");
+document.querySelector("select").addEventListener("change", function () {
+   let citySelected = document.querySelector("select").value;
+   document.querySelector("tbody").innerHTML = '';
    jadwalSholat(citySelected);
 });
